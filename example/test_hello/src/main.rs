6b91574cb97f74956484207f2a69d127e3a22e61
Rena <rena.sasaki@blueish.co.jp>
2021-07-16 17:21:20 +0900

    ディレクトリ内のファイルを表示

--- example/test_hello/src/main.rs
+++ example/test_hello/src/main.rs
@@ -1,6 +1,32 @@
-use rust_hello_test::{lib_hello, print_hello};
+// use rust_hello_test::{lib_hello, print_hello};
+
+// fn main() {
+//     lib_hello();
+//     print_hello();
+// }
+
+use std::fs;
+use std::io;
+use std::env;
+use std::path::{Path};
+
+fn read_dir<P: AsRef<Path>>(path: P) -> io::Result<Vec<String>> {
+  Ok(fs::read_dir(path)?
+      .filter_map(|entry| {
+          let entry = entry.ok()?;
+          if entry.file_type().ok()?.is_file() {
+            println!("{}",entry.file_name().to_string_lossy().into_owned());
+            Some(entry.file_name().to_string_lossy().into_owned())
+          } else {
+              None
+          }
+      })
+      .collect())
+}
 
 fn main() {
-    lib_hello();
-    print_hello();
+  let path = env::current_dir().unwrap();
+  println!("starting dir: {:?}", path.display());
+  let format_migrate = format!("{}/migration", path.display());
+  read_dir(format_migrate);
 }
--- src/bin/seeder.rs
+++ src/bin/seeder.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use std::io;
+use std::env;
+use std::path::{Path};
+
+fn read_dir<P: AsRef<Path>>(path: P) -> io::Result<Vec<String>> {
+  Ok(fs::read_dir(path)?
+      .filter_map(|entry| {
+          let entry = entry.ok()?;
+          if entry.file_type().ok()?.is_file() {
+            println!("{}",entry.file_name().to_string_lossy().into_owned());
+            Some(entry.file_name().to_string_lossy().into_owned())
+          } else {
+              None
+          }
+      })
+      .collect())
+}
+
 fn main() {
-  println!("seeder");
+  let path = env::current_dir().unwrap();
+  println!("starting dir: {:?}", path.display());
+  let format_migrate = format!("{}/migration", path.display());
+  read_dir(format_migrate).unwrap();
 }
